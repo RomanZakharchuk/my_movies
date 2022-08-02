19b3b5d9d2332e8ba8ab6ec9cccaed039819bd7a
Roman <70903505+RomanZakharchuk@users.noreply.github.com>
2022-08-02 20:19:16 +0300

    remove num page

--- src/components/MoviesListCards/MoviesListCards.js
+++ src/components/MoviesListCards/MoviesListCards.js
@@ -1,52 +1,51 @@
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useState } from "react";
-import { Pagination } from "@mui/material";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect, useState} from "react";
+import {Pagination} from "@mui/material";
 
-import { MoviesListCard } from "../../components";
+import {MoviesListCard} from "../../components";
 import style from "./MoviesListCards.module.scss";
-import { movieActive } from "../../redux/slices";
+import {movieActive} from "../../redux/slices";
 
 const MoviesListCards = () => {
-  const [page, setPage] = useState(1);
-  const [pageQty] = useState(500);
+    const [page, setPage] = useState(1);
+    const [pageQty] = useState(500);
 
-  const { movies } = useSelector((state) => state.movies);
-  const { selectedGenre } = useSelector((state) => state.movies);
-  // const { numberPage } = useSelector((state) => state.movies);
-  const dispatch = useDispatch();
+    const {movies} = useSelector((state) => state.movies);
+    const {selectedGenre} = useSelector((state) => state.movies);
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(movieActive.getMovieGenres(selectedGenre, page));
-  }, [dispatch, page, selectedGenre]);
+    useEffect(() => {
+        dispatch(movieActive.getMovieGenres(selectedGenre, page));
+    }, [dispatch, page, selectedGenre]);
 
-  const onPageChange = (num) => {
-    setPage(num);
-    dispatch(movieActive.getNumPage(num));
+    const onPageChange = (num) => {
+        setPage(num);
+        dispatch(movieActive.getNumPage(num));
 
-    const selectedCategory = Object.assign({}, selectedGenre);
-    selectedCategory.numberPage = num;
+        const selectedCategory = Object.assign({}, selectedGenre);
+        selectedCategory.numberPage = num;
 
-    dispatch(movieActive.getSelectedGenre(selectedCategory));
-  };
+        dispatch(movieActive.getSelectedGenre(selectedCategory));
+    };
 
-  return (
-    <div className={style.movie_list_cards}>
-      <div className={style.list_cards}>
-        {movies.map((movie, index) => (
-          <MoviesListCard key={index} movie={movie} />
-        ))}
-      </div>
+    return (
+        <div className={style.movie_list_cards}>
+            <div className={style.list_cards}>
+                {movies.map((movie, index) => (
+                    <MoviesListCard key={index} movie={movie}/>
+                ))}
+            </div>
 
-      <div className={style.pagination}>
-        <Pagination
-          count={pageQty}
-          page={page}
-          onChange={(_, num) => onPageChange(num)}
-          color="secondary"
-        />
-      </div>
-    </div>
-  );
+            <div className={style.pagination}>
+                <Pagination
+                    count={pageQty}
+                    page={page}
+                    onChange={(_, num) => onPageChange(num)}
+                    color="secondary"
+                />
+            </div>
+        </div>
+    );
 };
 
-export { MoviesListCards };
+export {MoviesListCards};
